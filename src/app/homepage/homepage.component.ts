30a93c6039e0be5037edbdf6d5e980cdd0dcd4e6
avrilquach <family.happy89@gmail.com>
2019-10-16 15:43:17 +0700

    'uphinh'

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,26 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-  array = [
-    {
-      guid: '900ea552-ef68-42cc-b6a6-b8c4dff10fb7',
-      age: 32,
-      name: 'Powers Schneider',
-    },
-    {
-      guid: '880381d3-8dca-4aed-b207-b3b4e575a15f',
-      age: 25,
-      name: 'Adrian Lawrence',
-    },
-    {
-      guid: '87b47684-c465-4c51-8c88-3f1a1aa2671b',
-      age: 32,
-      name: 'Boyer Stanley',
-    },
-  ]
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private api: ApiService,private http: Http) {
   }
-
+  smartphone: any = [];
+  ngOnInit() {
+    this.api.getSmartphone();
+  }
 }
