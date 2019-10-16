eeb2a826e7b182c2d54bf0cd458387fe71df2772
avril quach <flaxnguyen@gmail.com>
2019-10-16 09:08:09 -0600

    upfile

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 const localUrl = 'assets/data/photo.json';
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,7 @@ const localUrl = 'assets/data/photo.json';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  getPhotos() {
+  getPhotos(): Observable<any> {
     return this.http.get(localUrl);
   }
 }
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
-import { HttpClient } from "@angular/common/http";
-import { Observable, throwError } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from '../api.service';
+import {NgModule} from '@angular/core';
+import {Ng2OrderModule} from 'ng2-order-pipe';
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+    selector: 'app-homepage',
+    templateUrl: './homepage.component.html',
+    styleUrls: ['./homepage.component.scss']
+})
+
+@NgModule({
+    imports: [Ng2OrderModule]
 })
 export class HomepageComponent implements OnInit {
 
-  constructor(private api: ApiService, private  http: HttpClient) {}
-  photos: any = [];
-  ngOnInit() {
-      this.api.getPhotos()
-        .subscribe(data => {
-          for(const d of (data as any)) {
-            this.photos.push({
-              id: d.id,
-              name: d.name,
-              src: d.src,
-              rating: d.rating
-            });
-          }
-          console.log(this.photos);
-        });
+    constructor(private api: ApiService) {
+    }
+
+    photos: any = [];
+
+    ngOnInit() {
+        this.api.getPhotos()
+            .subscribe(data => this.photos = data );
+        photos = _.orderBy(photos, 'photos.id', 'desc');
     }
 }
