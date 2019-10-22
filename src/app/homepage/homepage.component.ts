1fada287a8677b99f8a009532839a2b5e9e8cf51
avrilquach <family.happy89@gmail.com>
2019-10-22 17:19:52 +0700

    'upfile'

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../api.service';
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -10,8 +10,10 @@ import { Router } from '@angular/router';
 
 export class HomepageComponent implements OnInit {
   photos: any = [];
+
   constructor(private api: ApiService, public router: Router) {
   }
+
   ngOnInit() {
     this.AllPhotos();
   }
@@ -20,12 +22,18 @@ export class HomepageComponent implements OnInit {
     this.api.getPhotos()
       .subscribe(data => this.photos = data);
   }
-  UpdatePhotos(id: number,photos: any) {
-    if(window.confirm('Are you sure, you want to vote?')){
-      this.api.updatePhotos(id, photos).subscribe(data => {
-        this.AllPhotos();
-      })
+
+  UpdatePhotos(id: number, photos: any) {
+    this.api.updatePhotos(id, photos).subscribe(data => {
+      this.AllPhotos();
+    })
+  }
+
+  generateStar(star: number) {
+    for (var i = 1; i < star; i++) {
+      console.log(star);
     }
   }
+
 
 }
